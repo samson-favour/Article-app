8d88dfba91cff7ced8fbdca148fb42002b03f3fe
samson-favour <favoursamson347@gmail.com>
2022-05-09 12:01:01 -0700

    new changes

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
-const categoryRoute = require("./routes/categories");
+
 const multer = require("multer");
 const path = require("path");
 
@@ -18,7 +18,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify:true
+    useFindAndModify: true,
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
@@ -40,7 +40,6 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
   console.log("Backend is running.");
